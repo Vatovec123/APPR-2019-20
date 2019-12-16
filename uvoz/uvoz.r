a951e8548dbcaf930c190faa06cde81fdcb674a1
Vatovec123 <kristina.vatovec13@gmail.com>
2019-12-16 22:35:48 +0100

    uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -53,3 +53,13 @@ druzine <- uvozi.druzine(levels(obcine$obcina))
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+starost <- read_csv2("podatki/pogostost-starost.csv", col_names=c("LETO", "POGOSTOST","Moški1624", "Moški2554", "Moški5574","Ženske1624", "Ženske2554","Ženske5574"),
+                     skip = 2, n_max = 13, locale=locale(encoding = "Windows-1250"))
+
+izobrazba <- read_csv2("podatki/pogostost-izobrazba.csv", col_names=c("LETO", "POGOSTOST E-NAKUPOVANJA",
+                                                                      "Moški-vsaj OŠ izobrazba", "Moški-srednja izobrazba","Moški-vsaj višješolska izobrazba",
+                                                                      "Ženske-vsaj OŠ izobrazba", "Ženske-srednja izobrazba", "Ženske-vsaj višješolska izobrazba"),
+                       skip = 2, n_max = 13, locale=locale(encoding = "Windows-1250"))
+izdelek <- read_csv2("podatki/vrsta izdelka ali storitve.csv", col_names=c("LETO","E-NAKUPI PO VRSTI IZDELKA ALI STORITVE",
+                                                                           "16-24","25-34","35-44","45-54","55-64","65-74"),
+                     skip = 2, n_max = 234, na = "-", locale=locale(encoding = "Windows-1250"))

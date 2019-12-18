de97e4275c410562bc740948bb313edfa2e93acb
Vatovec123 <kristina.vatovec13@gmail.com>
2019-12-18 08:16:10 +0100

    uvoz

--- uvoz/uvoz.R
+++ uvoz/uvoz.R
@@ -1,1 +1,6 @@
 uvoz
+
+starost <- read_csv2("podatki/pogostost-starost.csv", col_names=c("LETO","POGOSTOST E-NAKUPOVANJA",
+                                                                "Moški 16-24","Moški 25-54","Moški 55-74",
+                                                                "Ženske 16-24", "Ženske 25-54","Ženske 55-74"),
+                     skip = 2, n_max = 13, locale=locale(encoding = "Windows-1250"))

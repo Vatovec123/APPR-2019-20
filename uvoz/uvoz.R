229324e4a4132b7ff61edced93ed7b9f43009f4b
Kristina Vatovec <kristina.vatovec13@gmail.com>
2019-12-19 10:14:09 +0100

    uvoz

--- uvoz/uvoz.R
+++ uvoz/uvoz.R
@@ -1,4 +1,6 @@
 uvoz
+sl <-locale("sl", decimal_mark = ";", grouping_mark = ".")
+
 
 starost <- read_csv2("podatki/pogostost-starost.csv", col_names=c("LETO","POGOSTOST E-NAKUPOVANJA",
                                                                 "Moški 16-24","Moški 25-54","Moški 55-74",
@@ -20,8 +22,12 @@ vrednost <- read_csv2("podatki/število in ocenjena vrednost-stopnja urbanizacij
                       col_names=c("LETO","ŠTEVILO IN VREDNOST SPLETNIH NAKUPOV","Gosto poseljena območja","Vmesna območja","Redko poseljena območja","Vzhodna Slovenija","Zahodna Slovenija"),
                       skip = 3, n_max = 48, locale=locale(encoding = "Windows-1250"))
 
+stolpci <- c("leto","iskanje",
+             "Moški-največ OŠ izobrazba","Moški-srednja izobrazba","Moški-vsaj višješolska izobrazba",
+             "Ženske-največ OŠ izobrazba","Ženske-srednja izobrazba","Ženske-vsaj višješolska izobrazba") %>%
+  iconv(to="UTF-8")
 informacija <- read_csv2("podatki/pogostost iskanja informacij pred nakupom.csv",
-                         col_names=c("LETO","ISKANJE INFORMACIJ NA INTERNETU",
-                                     "Moški-največ OŠ izobrazba","Moški-srednja izobrazba","Moški-vsaj višješolska izobrazba",
-                                     "Ženske-največ OŠ izobrazba","Ženske-srednja izobrazba","Ženske-vsaj višješolska izobrazba"),
-                         skip = 3, n_max = 13, locale=locale(encoding = "Windows-1250"))
+                         col_names=stolpci,
+                         skip = 3, n_max = 13, locale=locale(encoding = "Windows-1250")) %>%
+  gather(key="spol.izobrazba", value="stevilo", -leto, -iskanje) %>%
+  separate(spol.izobrazba, c("spol", "izobrazba"), "-")

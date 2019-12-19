f12d767afb9989bff38445a0dae79abb97794fc6
Kristina Vatovec <kristina.vatovec13@gmail.com>
2019-12-19 10:29:38 +0100

    uvoz

--- uvoz/uvoz.R
+++ uvoz/uvoz.R
@@ -13,10 +13,9 @@ izobrazba <- read_csv2("podatki/pogostost-izobrazba.csv",
                                    "Ženske-vsaj OŠ izobrazba","Ženske-vsaj srednja izobrazba","Ženske-vsaj višješolska izobrazba"),
                        skip = 3, n_max = 13, locale=locale(encoding = "Windows-1250"))
 
-izdelek <- read_csv2("podatki/vrsta izdelka ali storitve",
-                      col_names=c("LETO","E-NAKUPOVANJE PO VRSTI IZDELKA ALI STORITVE",
-                                  "16-24","25-34","35-44","55-64","65-74"),
-                      skip = 3, n_max = 234, na = "-", locale=locale(encoding = "Windows-1250"))
+izdelek <- read_csv2("podatki/vrsta izdelka ali storitve.csv",
+                      skip = 2, n_max = 234, na = "-", locale=locale(encoding = "Windows-1250")) %>%
+  rename(e.nakupi=`E-NAKUPI  PO VRSTI IZDELKA ALI STORITVE`)
 
 vrednost <- read_csv2("podatki/število in ocenjena vrednost-stopnja urbanizacije.csv",
                       col_names=c("LETO","ŠTEVILO IN VREDNOST SPLETNIH NAKUPOV","Gosto poseljena območja","Vmesna območja","Redko poseljena območja","Vzhodna Slovenija","Zahodna Slovenija"),
